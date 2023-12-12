a42167f931aad9e85e848781ae85e23f346f696e
Liberbell <liberbell@gmail.com>
2023-12-13 08:58:02 +0900

    bloomberg2

--- newspaper/bloomberg.py
+++ newspaper/bloomberg.py
@@ -3,3 +3,9 @@ import newspaper
 URL = "https://www.bloomberg.co.jp/"
 
 website = newspaper.build(URL)
+
+for article in website.articles:
+    article.download()
+    article.parse()
+    article.nlp()
+    print(article)
