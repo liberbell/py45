604ed051b869fdd9a805fcd9ad0e18b5cb19d9cf
Liberbell <liberbell@gmail.com>
2023-12-15 08:27:47 +0900

    newstest2

--- newspaper/newstest.py
+++ newspaper/newstest.py
@@ -12,3 +12,16 @@ URL2 = "https://www.itmedia.co.jp/"
 # print(website.text.replace("\n", ""))
 
 website = newspaper.build(URL, memoize_articles=False)
+
+i = 0
+for article in website.articles:
+    article.download()
+    article.parse()
+    article.nlp()
+    print(article.publish_date)
+    print(article.author)
+    print(article.text)
+
+    if i > 9:
+        break
+    i += 1
