541731500cbdf156e07e5aeeca02b8a58c09ef0a
Liberbell <liberbell@gmail.com>
2023-12-15 08:34:08 +0900

    newstest6

--- newspaper/newstest.py
+++ newspaper/newstest.py
@@ -3,6 +3,7 @@ import newspaper
 
 URL = "https://diamond-rm.net/overseas/american_economy/472725/"
 URL2 = "https://www.itmedia.co.jp/"
+URL3 = "https://thebridge.jp/"
 # website = Article(URL, memoize_articles=False)
 
 # website.download()
@@ -11,17 +12,20 @@ URL2 = "https://www.itmedia.co.jp/"
 # print(website.authors)
 # print(website.text.replace("\n", ""))
 
-website = newspaper.build(URL, memoize_articles=False)
+website = newspaper.build(URL2, memoize_articles=False)
+website2 = newspaper.build(URL3, memoize_articles=False)
+
 
 i = 0
-for article in website.articles:
-    article.download()
-    article.parse()
-    article.nlp()
+for article1 in website.articles:
+    article1.download()
+    article1.parse()
+    article1.nlp()
     print("Index: ", i)
-    print("Date: ", article.publish_date)
-    print("Author: ", article.authors)
-    print("Article: ", article.text)
+    print("Date: ", article1.publish_date)
+    print("URL: ", article1.url)
+    print("Author: ", article1.authors)
+    print("Article: ", article1.text)
 
     if i > 9:
         break
