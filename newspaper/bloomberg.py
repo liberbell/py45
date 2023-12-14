9d22c154a685acf713d07b7727b34b323cc64600
Liberbell <liberbell@gmail.com>
2023-12-14 13:11:22 +0900

    bloomberg2

--- newspaper/bloomberg.py
+++ newspaper/bloomberg.py
@@ -4,6 +4,7 @@ URL = "https://www.bloomberg.co.jp/"
 
 website = newspaper.build(URL, memoize_articles=False)
 
+i = 0
 for article in website.articles:
     article.download()
     article.parse()
@@ -11,3 +12,7 @@ for article in website.articles:
     print(article.title)
     print(article.url)
     print(article.summary, end="\n\n")
+
+    if i > 9:
+        break
+    i += 1
