d5a34260705549eba39426c10a58e16501a634b9
Liberbell <liberbell@gmail.com>
2023-12-14 13:39:06 +0900

    newstest3

--- newspaper/newstest.py
+++ newspaper/newstest.py
@@ -1,7 +1,8 @@
-import newspaper
+from newspaper import Article
 
-URL = "https://diamond-rm.net/"
-website = newspaper.build(URL, memoize_articles=False)
+URL = "https://diamond-rm.net/overseas/american_economy/472725/"
+website = Article(URL, memoize_articles=False)
 
 website.download()
 website.parse()
+print(website.publish_date)
