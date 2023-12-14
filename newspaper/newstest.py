e0bcf43ea299e032d9b21f5b4328bb6f75f6b0f5
Liberbell <liberbell@gmail.com>
2023-12-15 08:25:01 +0900

    newstest3

--- newspaper/newstest.py
+++ newspaper/newstest.py
@@ -1,10 +1,14 @@
 from newspaper import Article
+import newspaper
 
 URL = "https://diamond-rm.net/overseas/american_economy/472725/"
-website = Article(URL, memoize_articles=False)
+URL2 = "https://www.itmedia.co.jp/"
+# website = Article(URL, memoize_articles=False)
 
-website.download()
-website.parse()
-print(website.publish_date)
-print(website.authors)
-print(website.text.replace("\n", ""))
+# website.download()
+# website.parse()
+# print(website.publish_date)
+# print(website.authors)
+# print(website.text.replace("\n", ""))
+
+website = newspaper.build(URL, memoize_articles=False)
