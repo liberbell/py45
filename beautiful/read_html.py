6690039c054630730b60d93a63c4a93f6a466def
Liberbell <liberbell@gmail.com>
2023-12-15 09:05:21 +0900

    read_html4

--- beautiful/read_html.py
+++ beautiful/read_html.py
@@ -6,3 +6,4 @@ URL = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
 data = pd.read_html(requests.get(URL, headers={'User-agent': 'Mozilla/5.0'}).text, header=0)
 
 print(data[0].head())
+print(data[0].tail())
