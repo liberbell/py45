9bd6d41c1fae0a1e53d5542c5ec2412bb795dd82
Liberbell <liberbell@gmail.com>
2023-12-15 09:02:56 +0900

    read_html2

--- beautiful/read_html.py
+++ beautiful/read_html.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import requests
 
 
 URL = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
+data = pd.read_html(requests.get(URL, headers={'User-agent': 'Mozilla/5.0'}).text, header=0)
+
+print(data[0].head())
