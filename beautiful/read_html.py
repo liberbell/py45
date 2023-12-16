c5d22e49b13b01e992f26b551b3dbf17f4b49f25
Liberbell <liberbell@gmail.com>
2023-12-16 10:25:18 +0900

    read_html2

--- beautiful/read_html.py
+++ beautiful/read_html.py
@@ -10,3 +10,6 @@ print(data[0].head())
 
 data[0]["Adj Close**"] = pd.to_numeric(data[0]["Adj Close**"], errors="coerce")
 print(data[0].tail())
+
+data[0].dropna(inplace=True)
+print(data[0].tail())
