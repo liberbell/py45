5ab66c927d233c7db3fe4e3e37945d2d41b22f99
Liberbell <liberbell@gmail.com>
2023-12-16 10:34:11 +0900

    read_html4

--- beautiful/read_html.py
+++ beautiful/read_html.py
@@ -16,5 +16,8 @@ data[0].dropna(inplace=True)
 # print(data[0].tail())
 
 data[0]["Date2"] = [dt.strptime(i, "%b %d, %Y") for i in data[0]["Date"]]
-print(data[0]["Date2"].head())
+# print(data[0]["Date2"].head())
+# print(data[0].head())
+
+data[0].set_index("Date2", inplace=True)
 print(data[0].head())
