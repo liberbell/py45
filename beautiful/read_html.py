c18a4dbbd7ec3ca8802564de58be9d5db9fef7e1
Liberbell <liberbell@gmail.com>
2023-12-16 10:23:23 +0900

    read_html1

--- beautiful/read_html.py
+++ beautiful/read_html.py
@@ -6,4 +6,7 @@ URL = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
 data = pd.read_html(requests.get(URL, headers={'User-agent': 'Mozilla/5.0'}).text, header=0)
 
 print(data[0].head())
+
+
+data[0]["Adj Close**"] = pd.to_numeric(data[0]["Adj Close**"], errors="coerce")
 print(data[0].tail())
