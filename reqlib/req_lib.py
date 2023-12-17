267f3f038fb8c17e29d10dc8dda40e888990274b
Liberbell <liberbell@gmail.com>
2023-12-17 11:04:14 +0900

    req_lib1

--- reqlib/req_lib.py
+++ reqlib/req_lib.py
@@ -3,7 +3,11 @@ import requests
 URL = "https://www.yahoo.co.jp"
 
 response = requests.get(URL)
-print(response.status_code)
-print(response.text)
+# print(response.status_code)
+# print(response.text)
 
-print(response.encoding)
+# print(response.encoding)
+print(response.headers)
+
+for key, value in response.headers.items():
+    print(key, " ", value)
