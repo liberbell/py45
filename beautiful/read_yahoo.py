1d74f517c593f3a372eba54a7120c5f1085e2764
Liberbell <liberbell@gmail.com>
2023-12-18 08:56:42 +0900

    read_yahoo4

--- beautiful/read_yahoo.py
+++ beautiful/read_yahoo.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+import re
 
 URL = "https://www.yahoo.co.jp/"
 
@@ -8,4 +9,7 @@ response = requests.get(URL)
 soup = BeautifulSoup(response.text, "html.parser")
 
 elems = soup.find_all("a")
+# print(elems)
+
+elems = soup.find_all(href=re.compile("news.yahoo.co.jp/pickup"))
 print(elems)
