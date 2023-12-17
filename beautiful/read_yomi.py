185516ebd95aefdf447ef1702fc17c2d577a2078
Liberbell <liberbell@gmail.com>
2023-12-17 15:18:19 +0900

    read_yomi4

--- beautiful/read_yomi.py
+++ beautiful/read_yomi.py
@@ -8,3 +8,7 @@ print(response.status_code)
 print(response.text)
 soup = BeautifulSoup(response.text, "html.parser")
 print(soup)
+
+# body > div.home-2021-primary > div.home-2021-primary__main > div.headline > article:nth-child(1) > div > h3 > a
+# body > div.home-2021-primary > div.home-2021-primary__main > div.headline > article:nth-child(3) > div > h3 > a
+# body > div.home-2021-primary > div.home-2021-primary__main > div.headline > article:nth-child(6) > div > h3 > a
