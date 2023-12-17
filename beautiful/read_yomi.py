22ae7b6335693252d2ecffc13210cbf6ab8fcfd9
Liberbell <liberbell@gmail.com>
2023-12-17 15:12:54 +0900

    read_yomi2

--- beautiful/read_yomi.py
+++ beautiful/read_yomi.py
@@ -4,3 +4,7 @@ from bs4 import BeautifulSoup
 URL = "https://www.yomiuri.co.jp/"
 response = requests.get(URL)
 print(response.status_code)
+
+print(response.text)
+soup = BeautifulSoup(response.text, "html.parser")
+print(soup)
