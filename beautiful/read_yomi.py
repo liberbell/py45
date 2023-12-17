632f539647075af9ce43942ca83db472e5b4b15f
Liberbell <liberbell@gmail.com>
2023-12-17 15:19:44 +0900

    read_yomi5

--- beautiful/read_yomi.py
+++ beautiful/read_yomi.py
@@ -3,12 +3,15 @@ from bs4 import BeautifulSoup
 
 URL = "https://www.yomiuri.co.jp/"
 response = requests.get(URL)
-print(response.status_code)
+# print(response.status_code)
 
-print(response.text)
+# print(response.text)
 soup = BeautifulSoup(response.text, "html.parser")
-print(soup)
+# print(soup)
 
 # body > div.home-2021-primary > div.home-2021-primary__main > div.headline > article:nth-child(1) > div > h3 > a
 # body > div.home-2021-primary > div.home-2021-primary__main > div.headline > article:nth-child(3) > div > h3 > a
 # body > div.home-2021-primary > div.home-2021-primary__main > div.headline > article:nth-child(6) > div > h3 > a
+
+elems = soup.select("div.headline > article:nth-child(1) > div > h3 > a")
+print(elems[0])
