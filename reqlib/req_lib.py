40b09c8f9f1c689ba6621520e5b7e73956e7cad0
Liberbell <liberbell@gmail.com>
2023-12-17 14:43:03 +0900

    req_lib3

--- reqlib/req_lib.py
+++ reqlib/req_lib.py
@@ -2,7 +2,7 @@ import requests
 
 URL = "https://www.yahoo.co.jp"
 
-response = requests.get(URL)
+# response = requests.get(URL)
 # print(response.status_code)
 # print(response.text)
 
@@ -15,3 +15,6 @@ response = requests.get(URL)
 print(response.cookies)
 
 user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+header = ["user-agent": user_agent]
+
+response = requests.get(URL, headers=header)
