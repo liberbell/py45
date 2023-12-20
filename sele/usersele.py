30f8045a5df3c429f965f2ef2cc7e2ac7bd6f56d
Liberbell <liberbell@gmail.com>
2023-12-20 09:00:21 +0900

    usersele3

--- sele/usersele.py
+++ sele/usersele.py
@@ -3,5 +3,5 @@ from time import sleep
 
 URL1 = "https://www.google.com/search?q=python&oq=python&gs_lcrp=EgZjaHJvbWUyDggAEEUYORhDGIAEGIoFMg4IARBFGCcYOxiABBiKBTIGCAIQRRg8MgYIAxBFGDwyBggEEEUYPDIGCAUQRRhBMgYIBhBFGEEyBggHEEUYQdIBCDM3MDNqMGo0qAIAsAIA&sourceid=chrome&ie=UTF-8#ip=1"
 
-driver = webdriver.Chrome("chromedriver")
+driver = webdriver.Chrome()
 driver.get(URL1)
