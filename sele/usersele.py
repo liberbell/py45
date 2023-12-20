fb5d5272d56992214b05b2bd37a1a0b1c9c6be8a
Liberbell <liberbell@gmail.com>
2023-12-20 22:29:44 +0900

    usersele4

--- sele/usersele.py
+++ sele/usersele.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.safari.options import Options
+# from selenium.webdriver.safari.options import Options
+from selenium.webdriver.chrome.options import Options
 from time import sleep
 
 URL1 = "https://www.google.com/search?q=python&oq=python&gs_lcrp=EgZjaHJvbWUyDggAEEUYORhDGIAEGIoFMg4IARBFGCcYOxiABBiKBTIGCAIQRRg8MgYIAxBFGDwyBggEEEUYPDIGCAUQRRhBMgYIBhBFGEEyBggHEEUYQdIBCDM3MDNqMGo0qAIAsAIA&sourceid=chrome&ie=UTF-8#ip=1"
 URL2 = "https://www.google.com"
 
-# driver = webdriver.Chrome()
-driver = webdriver.Safari()
-safari_options = Options()
-safari_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome()
+# driver = webdriver.Safari()
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 driver.get(URL2)
 
 search_bar = driver.find_element(By.NAME, "q")
