458da0449cce2ffc70fecb302dbe6e2aa34278cc
Liberbell <liberbell@gmail.com>
2023-12-20 09:04:42 +0900

    usersele5

--- sele/usersele.py
+++ sele/usersele.py
@@ -4,5 +4,6 @@ from time import sleep
 URL1 = "https://www.google.com/search?q=python&oq=python&gs_lcrp=EgZjaHJvbWUyDggAEEUYORhDGIAEGIoFMg4IARBFGCcYOxiABBiKBTIGCAIQRRg8MgYIAxBFGDwyBggEEEUYPDIGCAUQRRhBMgYIBhBFGEEyBggHEEUYQdIBCDM3MDNqMGo0qAIAsAIA&sourceid=chrome&ie=UTF-8#ip=1"
 URL2 = "https://www.google.com"
 
-driver = webdriver.Chrome()
+# driver = webdriver.Chrome()
+driver = webdriver.Safari()
 driver.get(URL2)
