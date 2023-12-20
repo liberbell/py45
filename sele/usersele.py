17d27d208996b918ad9c26f274c9d001fd3ac7ea
Liberbell <liberbell@gmail.com>
2023-12-20 22:28:19 +0900

    usersele3

--- sele/usersele.py
+++ sele/usersele.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.safari.options import Options
 from time import sleep
 
 URL1 = "https://www.google.com/search?q=python&oq=python&gs_lcrp=EgZjaHJvbWUyDggAEEUYORhDGIAEGIoFMg4IARBFGCcYOxiABBiKBTIGCAIQRRg8MgYIAxBFGDwyBggEEEUYPDIGCAUQRRhBMgYIBhBFGEEyBggHEEUYQdIBCDM3MDNqMGo0qAIAsAIA&sourceid=chrome&ie=UTF-8#ip=1"
@@ -7,7 +8,13 @@ URL2 = "https://www.google.com"
 
 # driver = webdriver.Chrome()
 driver = webdriver.Safari()
+safari_options = Options()
+safari_options.add_experimental_option("detach", True)
 driver.get(URL2)
 
 search_bar = driver.find_element(By.NAME, "q")
 search_bar.send_keys("python")
+
+search_bar.submit()
+
+driver.quit()
