773947e3673314a321f7f3c1c3860ac935e4e8a3
Liberbell <liberbell@gmail.com>
2023-12-20 09:02:34 +0900

    usersele4

--- sele/usersele.py
+++ sele/usersele.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from time import sleep
 
 URL1 = "https://www.google.com/search?q=python&oq=python&gs_lcrp=EgZjaHJvbWUyDggAEEUYORhDGIAEGIoFMg4IARBFGCcYOxiABBiKBTIGCAIQRRg8MgYIAxBFGDwyBggEEEUYPDIGCAUQRRhBMgYIBhBFGEEyBggHEEUYQdIBCDM3MDNqMGo0qAIAsAIA&sourceid=chrome&ie=UTF-8#ip=1"
+URL2 = "https://www.google.com"
 
 driver = webdriver.Chrome()
-driver.get(URL1)
+driver.get(URL2)
