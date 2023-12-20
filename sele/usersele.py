f50f3f28818af5bc4fdd4120f8c6db1d9865efef
Liberbell <liberbell@gmail.com>
2023-12-21 08:49:34 +0900

    usersele7

--- sele/usersele.py
+++ sele/usersele.py
@@ -9,16 +9,16 @@ URL2 = "https://www.google.com"
 
 driver = webdriver.Chrome()
 # driver = webdriver.Safari()
-# chrome_options = Options()
-# chrome_options.add_experimental_option("detach", True)
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 driver.get(URL2)
 
 search_bar = driver.find_element(By.NAME, value="q")
 search_bar.send_keys("python")
 
 search_bar.submit()
-for elem_h3 in driver.find_elements(by=By.XPATH, value="//a/h3"):
-    print(elem_h3.text)
+for elem_h3 in driver.find_elements(By.XPATH, value="//a/h3"):
+    print(elem_h3)
     elem_a = elem_h3.find_element(by=By.XPATH, value="..")
     print(elem_a.get_attribute("href"))
 
