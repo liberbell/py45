f13205f0f74afe459b6bf7981a6efa1c365c4e91
Liberbell <liberbell@gmail.com>
2023-12-22 08:52:22 +0900

    web_login3

--- insta/web_login.py
+++ insta/web_login.py
@@ -1,10 +1,12 @@
 from selenium import webdriver
 from time import sleep
 import configparser
+from selenium.webdriver.chrome.options import Options
 
 
 config_txt = configparser.ConfigParser()
 config_txt_path = "config.txt"
+URL1 = "https://www.instagram.com/"
 
 with open(config_txt_path, encoding='utf-8') as fp:
     config_txt.read_file(fp)
@@ -12,3 +14,9 @@ with open(config_txt_path, encoding='utf-8') as fp:
     read_default = config_txt['Login']
     user_id = read_default.get('User_id')
     password = read_default.get("Password")
+
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(URL1)
