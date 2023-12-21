b67ae4e596ca3f779a9de4b1dc7e9a0a85306146
Liberbell <liberbell@gmail.com>
2023-12-22 08:47:51 +0900

    web_login2

--- insta/web_login.py
+++ insta/web_login.py
@@ -4,4 +4,11 @@ import configparser
 
 
 config_txt = configparser.ConfigParser()
-config_txt.read('config.txt', encoding='utf-8')
+config_txt_path = "config.txt"
+
+with open(config_txt_path, encoding='utf-8') as fp:
+    config_txt.read_file(fp)
+
+    read_default = config_txt['Login']
+    user_id = read_default.get('User_id')
+    print(user_id)
