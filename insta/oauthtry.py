26d3900fd6e73d0618b9b6f5e824da07541d8893
Liberbell <liberbell@gmail.com>
2023-12-23 14:48:34 +0900

    oauthtry2

--- insta/oauthtry.py
+++ insta/oauthtry.py
@@ -1,4 +1,14 @@
 import subprocess
+import configparser
+
+config_txt = configparser.ConfigParser()
+config_txt_path = "config.txt"
+
+with open(config_txt_path, encoding='utf-8') as fp:
+    config_txt.read_file(fp)
+
+    read_default = config_txt['Login']
+    oauth = read_default.get('User_id')
 
 two_step_authentication = ['oathtool', '--totp', '--base32', 'アカウント追加用のキー']
 print(subprocess.check_output(two_step_authentication))
