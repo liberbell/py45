f40dababd18f064d7f6b799040fb69a20498f5b1
Liberbell <liberbell@gmail.com>
2023-12-23 15:01:33 +0900

    oauthtry7

--- insta/oauthtry.py
+++ insta/oauthtry.py
@@ -10,6 +10,7 @@ with open(config_txt_path, encoding='utf-8') as fp:
 
     read_default = config_txt['Login']
     oauth = read_default.get('oauth')
+    print(oauth)
 
 
 cmd = ['oathtool', '--totp', '--base32', oauth]
@@ -20,6 +21,6 @@ print(string_2fa)
 
 two_step_authentication = ['oathtool', '--totp', '--base32', oauth]
 SecondLoginPass = re.findall(r'\d+', subprocess.check_output(two_step_authentication).decode('utf-8'))
-print(subprocess.check_output(two_step_authentication))
+# print(subprocess.check_output(two_step_authentication))
 print(SecondLoginPass[0])
 
