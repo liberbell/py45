9cb2b59bb1a768897b65713e368a7764b8b0892b
Liberbell <liberbell@gmail.com>
2023-12-23 14:49:14 +0900

    oauthtry3

--- insta/oauthtry.py
+++ insta/oauthtry.py
@@ -8,7 +8,7 @@ with open(config_txt_path, encoding='utf-8') as fp:
     config_txt.read_file(fp)
 
     read_default = config_txt['Login']
-    oauth = read_default.get('User_id')
+    oauth = read_default.get('oauth')
 
-two_step_authentication = ['oathtool', '--totp', '--base32', 'アカウント追加用のキー']
+two_step_authentication = ['oathtool', '--totp', '--base32', oauth]
 print(subprocess.check_output(two_step_authentication))
