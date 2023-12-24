93294480f317c559f1396b761a44b341a539266d
Liberbell <liberbell@gmail.com>
2023-12-24 10:04:45 +0900

    googlescroll5

--- scroll/googlescroll.py
+++ scroll/googlescroll.py
@@ -1,16 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from time import sleep
 
 URL1 = "https://www.google.com"
 
 chrome_options = Options()
 chrome_options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=chrome_options)
-driver.get(URL1)
 
 search_bar = driver.find_element(By.NAME, value="q")
 search_bar.send_keys("python")
 search_bar.submit()
+
+error_flag = False
+if error_flag is False:
+    try:
+        driver.get(URL1)
+        sleep(2)
 
 # driver.quit()
