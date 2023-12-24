1296cfc8c375ef914cf43f32a11b66f17e68c535
Liberbell <liberbell@gmail.com>
2023-12-24 09:59:24 +0900

    googlescroll2

--- scroll/googlescroll.py
+++ scroll/googlescroll.py
@@ -1,2 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+URL1 = "https://www.google.com"
+
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+driver.get(URL1)
